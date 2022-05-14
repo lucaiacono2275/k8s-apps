e449a93fd744575099230b6810f17349eb3d05fa
lucaiacono2275 <luca.iacono@gmail.com>
2022-05-14 12:20:07 +0200

    feat: added nextcloud

---
+++ nextcloud/argocd/script.sh
@@ -0,0 +1,1 @@
+argocd app create nextcloud --repo https://github.com/lucaiacono2275/k8s-apps --path nextcloud/helmfile --dest-server https://kubernetes.default.svc --dest-namespace nextcloud --config-management-plugin helmfile
