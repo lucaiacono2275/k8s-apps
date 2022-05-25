5f95a0d66f29d45c4111ce1b5c6317706a0a726d
lucaiacono2275 <luca.iacono@gmail.com>
2022-05-25 22:14:28 +0200

    fix: https settings

--- nextcloud/argocd/script.sh
+++ nextcloud/argocd/script.sh
@@ -1,1 +1,1 @@
-argocd app create nextcloud --repo https://github.com/lucaiacono2275/k8s-apps --path nextcloud/helmfile --dest-server https://kubernetes.default.svc --dest-namespace nextcloud --config-management-plugin helmfile
+argocd app create nextcloud --repo https://github.com/lucaiacono2275/k8s-apps --path nextcloud/helmfile --grpc-web --dest-server https://kubernetes.default.svc --dest-namespace nextcloud --config-management-plugin helmfile
